2d1eed3bc1143470965e3b2a113ac7e47d40ae10
Olvolo <olvolo17@gmail.com>
2023-06-23 16:56:35 +0300

    Жизнь животных в динамике

--- src/main/java/com/javarush/island/voloshanovskyi/Main.java
+++ src/main/java/com/javarush/island/voloshanovskyi/Main.java
@@ -1,7 +1,26 @@
 package com.javarush.island.voloshanovskyi;
 
+import java.util.Random;
+import com.javarush.island.voloshanovskyi.island_lokation.Cell;
+import static com.javarush.island.voloshanovskyi.Services.GlobalProceses.creationOfPoolTreadLifeCircule;
+import static com.javarush.island.voloshanovskyi.Services.Services.*;
+import static com.javarush.island.voloshanovskyi.island_lokation.Island.ISLAND_DEPTH;
+import static com.javarush.island.voloshanovskyi.island_lokation.Island.ISLAND_WIDTH;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Заселяем остров поклеточно рандомным количеством животных с рандомным весом и травой
+        Cell[][] cells = new Cell[ISLAND_WIDTH][ISLAND_DEPTH];
+        Random random = new Random();
+        settlingTheIslandWithAnimalsAndGrass(cells, random);
+
+        // Вывод данных о животных в ячейках
+        printAnimalConditions(cells);
+
+        // Создание пула потоков с ограничением по времени исполнения и запуск всех методов
+        creationOfPoolTreadLifeCircule(cells);
+
+        // Вывод данных о животных в ячейках после обработки
+        printAnimalConditions(cells);
     }
 }
